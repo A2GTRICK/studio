c65db542296a80ae87e11d95ea4987a922aee158
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-07-19 10:12:39 +0000

    Sign Up Failed Configuration error: The Firebase API key is invalid. Ple

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,10 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+if (!process.env.NEXT_PUBLIC_FIREBASE_API_KEY) {
+    throw new Error("Firebase API Key is not defined. Please check your .env file.");
+}
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
