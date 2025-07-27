f3d77dbe7f1760750436367458dea37babd4c41e
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-07-27 11:19:33 +0000

    Authentication Failed Configuration error: The Firebase API key is inval

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
+  plugins: [googleAI({apiKey: "AIzaSyCjxpXINGBgKj1nJj84hEAeLdQc4kIH6YE"})],
   model: 'googleai/gemini-1.5-flash-latest',
 });
