e7bca0263a2e400f57a16a9bbd717bc194609c4e
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-07-28 06:37:50 +0000

    Action Required! To finalize the change, please update the 'leadMagnetPa

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -1,12 +1,11 @@
-
 /**
  * @fileOverview Central configuration for marketing-related settings.
  */
 
 /**
  * The path to the lead magnet file that users receive upon subscribing to the newsletter.
- * This path should point to a file within the `public` directory.
+ * This path should point to a file within the `public` directory or be a full URL.
  * 
- * To change the file, upload a new one to `public/assets` and update the string below.
+ * To change the file, update the string below.
  */
-export const leadMagnetPath = '/assets/Top-20-Pharmacology-Questions.pdf';
+export const leadMagnetPath = 'https://drive.google.com/file/d/1N-YTeLQXYGv8yShWsZDh6y_ETkg9WJRi/view?usp=drivesdk';
