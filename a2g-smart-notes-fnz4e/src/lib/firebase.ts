bf57af6a541d88556cfda7421745691d4d7dad99
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-07-29 16:09:20 +0000

    Skip to content Navigation Menu New repository
    Type / to search
    Create

--- a2g-smart-notes-fnz4e/src/lib/firebase.ts
+++ a2g-smart-notes-fnz4e/src/lib/firebase.ts
@@ -1,23 +1,24 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// This configuration is now hardcoded to ensure stability.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCjxpXINGBgKj1nJj84hEAeLdQc4kIH6YE",
-  authDomain: "a2g-smart-notes-fnz4e.firebaseapp.com",
-  projectId: "a2g-smart-notes-fnz4e",
-  storageBucket: "a2g-smart-notes-fnz4e.appspot.com",
-  messagingSenderId: "287880847881",
-  appId: "1:287880847881:web:f178409a464922c309ced8"
+  apiKey: "AIzaSyA_UmQOO-k_unjL_lWFjtWuXpK_W824LsU",
+  authDomain: "a2g-final-version.firebaseapp.com",
+  projectId: "a2g-final-version",
+  storageBucket: "a2g-final-version.appspot.com",
+  messagingSenderId: "398487697373",
+  appId: "1:398487697373:web:cc482234732baa42e0e834",
+  measurementId: "G-89SEFZPXLS"
 };
 
 // Initialize Firebase for SSR
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
-
 
 export { app, db, auth };
