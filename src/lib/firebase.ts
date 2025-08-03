87210b8f54f8b102de1e6992fb9bf71eb04481da
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-08-03 12:20:05 +0000

    why nor publish button working" Publish your app
    Follow these steps to

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,21 +6,22 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDfO5BYIxKFfKJLtVcY2u7IwocJRWMuDyk",
-  authDomain: "a2g-smartprep-web.firebaseapp.com",
-  databaseURL: "https://a2g-smartprep-web-default-rtdb.firebaseio.com",
-  projectId: "a2g-smartprep-web",
-  storageBucket: "a2g-smartprep-web.appspot.com",
-  messagingSenderId: "119647035955",
-  appId: "1:119647035955:web:32d6877563c74cd40a7946",
-  measurementId: "G-KR7ER86LVR"
-};
+// Your web app's Firebase configuration will be injected automatically.
+
+const firebaseConfig = {};
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+let app;
+if (!getApps().length) {
+  try {
+    app = initializeApp(firebaseConfig);
+  } catch (e) {
+    console.error("Failed to initialize Firebase", e);
+  }
+} else {
+  app = getApp();
+}
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
