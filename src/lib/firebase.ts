07560bb6e1d64576537f885a34626b71ed75f866
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-11-28 20:23:30 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
@@ -6,12 +5,13 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 
 // All these values must be set as environment vars in Firebase Studio
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAj5hjPVD5klr6galuSkk0gdZ8Wd7l66l8",
+  authDomain: "a2g-smart-notes-1st.firebaseapp.com",
+  databaseURL: "https://a2g-smart-notes-1st-default-rtdb.firebaseio.com",
+  projectId: "a2g-smart-notes-1st",
+  storageBucket: "a2g-smart-notes-1st.appspot.com",
+  messagingSenderId: "593098306784",
+  appId: "1:593098306784:web:bbf1511a890e423c0e4c85"
 };
 
 let app: FirebaseApp;
