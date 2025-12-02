b1cb1950b1d1e4cf42dc2e217fdd72a4073894da
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-12-02 12:01:38 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 // src/firebase/config.ts
-
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-// Your Firebase web config (from Firebase Console)
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAj5hjPVD5klr6galuSkk0gdZ8Wd7l66l8",
   authDomain: "a2g-smart-notes-1st.firebaseapp.com",
@@ -13,7 +12,8 @@ const firebaseConfig = {
   projectId: "a2g-smart-notes-1st",
   storageBucket: "a2g-smart-notes-1st.appspot.com",
   messagingSenderId: "593098306784",
-  appId: "1:593098306784:web:bbf1511a890e423c0e4c85",
+  appId: "1:593098306784:web:e534cab5ec68ae820e4c85",
+  measurementId: "G-Z2K1QYDZR9"
 };
 
 // Prevent duplicate initialization
