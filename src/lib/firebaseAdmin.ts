55747fc3468da2fa7a70882ca1b926ceb4951064
Arvind Sharma <sharmaarvind28897@gmail.com>
2025-12-11 21:50:44 +0000

    WHAT NEXT ?

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,3 +1,4 @@
+
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
@@ -6,7 +7,7 @@ import { getStorage } from "firebase-admin/storage";
 const serviceAccount = {
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\n/g, '\n'),
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 }
 
 function getAdminApp() {
